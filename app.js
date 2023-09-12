305c726d2c6709d5a3204944c394a75f1f536ed0
TheIdolJavox <142756684+TheIdolJavox@users.noreply.github.com>
2023-09-12 12:44:56 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error interno del servidor');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/app.js') {
+        fs.readFile(path.join(__dirname, 'app.js'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error interno del servidor');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Página no encontrada');
+    }
+});
+
+const puerto = 3000;
+server.listen(puerto, () => {
+    console.log(`Servidor Node.js en funcionamiento en http://localhost:${puerto}`);
+});
